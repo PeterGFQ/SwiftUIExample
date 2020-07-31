75992a1d333945463f0d225811d0c74da28f1382
Peter Guo <peterguo@sph.com.sg>
2020-07-31 19:09:05 +0800

    Project creation

---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by peterguo on 31/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
